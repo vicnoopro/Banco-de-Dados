2f17bf1278d098432d1eb9baaf8b7b96ae1a7576
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-07-24 08:28:26 -0300

    criar tabelas virtuais

--- Banco de dados/2_trimestre/8_regra_codd_independencia_logica_dados.sql
+++ Banco de dados/2_trimestre/8_regra_codd_independencia_logica_dados.sql
@@ -9,6 +9,12 @@ date_format(dataAdmissao, "%d/%m/%Y") as "Data do Contrato",
 floor(datediff(curdate(),dataAdmissao)/365) as "Anos Trabalhados"
 from funconario);
 
+/*Na criaçã da view o ideal é montar o select que fará o filtro de dados 
+primeiro e em seguida, delimitá-lo por 'CREATE VIEW nomeTabelavitual as'
+e o comando select entre ().
+Cada view pode ser criada com o mesmo nomeTabelavirtual uma vez apenas.
+Feito isso, a view aparecerá na lista de tabelas com show tables ou na lateral esquerda, no objeto views.*/
+
 show tables;
 
 select * from vi_anosContrato;
