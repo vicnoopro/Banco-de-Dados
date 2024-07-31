3a0f6395178ed04392ddc3644d654e7150d77c6a
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-07-31 08:33:39 -0300

    aula 31/07

---
+++ Banco de dados/2_trimestre/exercicio_autoescola.sql
@@ -0,0 +1,31 @@
+
+##Regra 7 : Independencia física dos dados
+
+show tables;
+select * from carro;
+select * from marca;
+
+
+ALTER TABLE `autoescola2d`.`carro` 
+CHANGE COLUMN `anoFabricacao` `anodeproducao` YEAR NOT NULL;
+
+ALTER TABLE `autoescola2d`.`carro` 
+ADD COLUMN `corcarro` VARCHAR(45) NULL AFTER `modelo`;
+
+desc carro;
+desc marca;
+
+UPDATE `autoescola2d`.`carro` SET `corcarro` = 'Preto' WHERE (`cod_carro` = '1') and (`marca_codMarca` = '1');
+
+
+#Regra 8 : Independencia lógica dos dados
+create view vi_relatorioCompleto as(
+select m.nome as  "Marca do carro",
+ c.modelo as "Modelo", 
+c.corcarro as "Cor do Carro",
+ c.chassi as "Chassi",
+ c.anodeproducao as "Ano de Produção" from carro as c
+ inner join marca as m on c.marca_codMarca = m.codmarca);
+
+select * from vi_relatorioCompleto;
+
