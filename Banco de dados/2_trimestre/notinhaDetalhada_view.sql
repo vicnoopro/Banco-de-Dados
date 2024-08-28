4017e54d342d41d299bf866a63df95c8fd75cb83
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-08-28 08:01:21 -0300

    aula 2208

--- Banco de dados/2_trimestre/notinhaDetalhada_view.sql
+++ Banco de dados/2_trimestre/notinhaDetalhada_view.sql
@@ -12,5 +12,7 @@ create view vi_notinha as (
 select iv.venda_codVenda, p.nome, iv.quantVenda as "Quantidade Vendida", format(p.precoVenda,2) as "Preço de venda", format(iv.quantVenda * p.precoVenda,2) as "Valor a ser pago" from produto as p 
 inner join itensVenda as iV on p.codProduto = iV.produto_codProduto);
 
+select * from vi_notinha;
+
 
 
