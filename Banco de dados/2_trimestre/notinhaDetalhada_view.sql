2cde6ab105b6888ae93290db53504348f0feac84
Victoria Caixeta <victoriarabelocaixeta28@gmail.com>
2024-08-29 10:54:23 -0300

    aula 2908

--- Banco de dados/2_trimestre/notinhaDetalhada_view.sql
+++ Banco de dados/2_trimestre/notinhaDetalhada_view.sql
@@ -15,9 +15,13 @@ inner join itensVenda as iV on p.codProduto = iV.produto_codProduto);
 select * from vi_notinha;
 
 
-select v.codVenda, c.nomeCliente, v.tipoPagamento from venda as v inner join
-inner join produto as p,
- cliente as c on v.cliente_codCliente= c.codCliente;
+/*No projeto da Lanchonete, criar uma VIEW (vi_totalPagarPorVenda) que mostre o código de cada
+venda, o nome do respectivo cliente, o valor total a pagar e o método de pagamento.*/
 
+create view vi_totalPagarporVenda as(
+select v.codVenda, c.nomeCliente, v.tipoPagamento, sum(format(iv.quantVenda * p.precoVenda,2)) as "Total da Compra" from venda as v inner join cliente as c on v.cliente_codCliente = c.codCliente 
+inner join itensVenda as iV on iv.venda_codVenda = v.codVenda inner join produto as p on p.codProduto = iv.produto_codProduto group by v.codVenda, c.nomeCliente, v.tipoPagamento);
+
+select * from vi_totalPagarporVenda;
 
 
